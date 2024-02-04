fafbe5b6d408b6be863fb8086b1cf1988d2d6018
AtesiT <atesit@mail.ru>
2024-02-04 23:48:10 +0300

    replace grid on place in tki_messenger.py backup(grid) in messenger.py

---
+++ messenger.py
@@ -0,0 +1,50 @@
+#backup tki_messenger
+
+# from tkinter import *
+# from tkinter import ttk
+# import settings_messenger
+# import profile_messenger
+# import createNewDialog_messenger
+#
+# root = Tk()
+# root.title("Messenger")
+# root.geometry("350x600")
+#
+# class AnyDialog:
+#     def __init__(self, name, text, time):
+#         name = ttk.Label(text=f"{name}", font=("Century Gothic", 10))
+#         name.grid(row=1,column=0)
+#         text = ttk.Label(text=f"{text}", font=("Century Gothic", 14))
+#         text.grid(row=1,column=1)
+#         time = ttk.Label(text=f"{str(time)}", font=("Century Gothic", 10))
+#         time.grid(row=1,column=2)
+#
+# class UpperPartMesssenger:
+#     def __init__(self):
+#         settings = ttk.Button(text="Settings", command=settings_messenger.settings)
+#         settings.grid(row=0,column=0)
+#         profile = ttk.Button(text="Profile", command=profile_messenger.profile)
+#         profile.grid(row=0,column=2)
+#
+# class BottomPartMesssenger:
+#     def __init__(self):
+#         ttk.Entry().grid(row=2,column=0, columnspan=2, stick='we')
+#         ttk.Button(text='SEND').grid(row=2,column=2)
+#
+# class BottomToolbar:
+#     def __init__(self):
+#         settings = ttk.Button(text="MUSIC")
+#         settings.grid(row=3,column=0, stick='we')
+#         profile = ttk.Button(text="CREATE NEW DIALOG", command=createNewDialog_messenger.newDialog)
+#         profile.grid(row=3,column=1, stick='we')
+#         settings = ttk.Button(text="BACKUP")
+#         settings.grid(row=3,column=2, stick='we')
+#
+#
+#
+# UpperPartMesssenger()
+# AnyDialog('Alice', 'Hi, How\'s going on?', '14:17')
+# BottomPartMesssenger()
+# BottomToolbar()
+#
+# root.mainloop()
--- tki_messenger.py
+++ tki_messenger.py
@@ -11,37 +11,37 @@ root.geometry("350x600")
 class AnyDialog:
     def __init__(self, name, text, time):
         name = ttk.Label(text=f"{name}", font=("Century Gothic", 10))
-        name.grid(row=1,column=0)
+        name.place(x=10, y=50)
         text = ttk.Label(text=f"{text}", font=("Century Gothic", 14))
-        text.grid(row=1,column=1)
+        text.place(x=70, y=50)
         time = ttk.Label(text=f"{str(time)}", font=("Century Gothic", 10))
-        time.grid(row=1,column=2)
+        time.place(x=270, y=50)
 
 class UpperPartMesssenger:
     def __init__(self):
         settings = ttk.Button(text="Settings", command=settings_messenger.settings)
-        settings.grid(row=0,column=0)
+        settings.place(x=10, y=10)
         profile = ttk.Button(text="Profile", command=profile_messenger.profile)
-        profile.grid(row=0,column=2)
+        profile.place(x=250, y=10)
 
 class BottomPartMesssenger:
     def __init__(self):
-        ttk.Entry().grid(row=2,column=0, columnspan=2, stick='we')
-        ttk.Button(text='SEND').grid(row=2,column=2)
+        ttk.Entry().place(x=10, y=500)
+        ttk.Button(text='SEND').place(x=250, y=500)
 
 class BottomToolbar:
     def __init__(self):
         settings = ttk.Button(text="MUSIC")
-        settings.grid(row=3,column=0, stick='we')
+        settings.place(x=10, y=550)
         profile = ttk.Button(text="CREATE NEW DIALOG", command=createNewDialog_messenger.newDialog)
-        profile.grid(row=3,column=1, stick='we')
+        profile.place(x=100, y=550)
         settings = ttk.Button(text="BACKUP")
-        settings.grid(row=3,column=2, stick='we')
+        settings.place(x=250, y=550)
 
 
 
 UpperPartMesssenger()
-first_dialog = AnyDialog('Alice', 'Hi, How\'s going on?', '14:17')
+AnyDialog('Alice', 'Hi, How\'s going on?', '14:17')
 BottomPartMesssenger()
 BottomToolbar()
 
