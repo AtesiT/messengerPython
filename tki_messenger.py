d1aaecf163d76ac4ab9de2aec7e1a172766fd652
AtesiT <atesit@mail.ru>
2024-02-06 22:14:48 +0300

    added a restriction on characters in the nickname and message text. also amount lines added

--- tki_messenger.py
+++ tki_messenger.py
@@ -6,16 +6,29 @@ import createNewDialog_messenger
 
 root = Tk()
 root.title("Messenger")
-root.geometry("350x600")
+root.geometry("340x600")
+
 
 class AnyDialog:
-    def __init__(self, name, text, time):
-        name = ttk.Label(text=f"{name}", font=("Century Gothic", 10))
-        name.place(x=10, y=50)
-        text = ttk.Label(text=f"{text}", font=("Century Gothic", 14))
-        text.place(x=70, y=50)
-        time = ttk.Label(text=f"{str(time)}", font=("Century Gothic", 10))
-        time.place(x=270, y=50)
+    def __init__(self, name, text, time, numberLines):
+        self.name = name
+        self.text = text
+        self.time = time
+        self.numberLines = numberLines
+        everyheightline = 0
+        # print((lambda text: text[0:25] if len(text) > 25 else text)(text))
+        if len(self.text) > 20:
+            self.text = self.text[0:20] + '..'
+        if len(self.name) > 7:
+            self.name = self.name[0:7] + '..'
+        for i in range(numberLines):
+            name = ttk.Label(text=f"{self.name}", font=("Century Gothic", 10))
+            name.place(x=10, y=50 + everyheightline)
+            text = ttk.Label(text=f"{self.text}", font=("Century Gothic", 14))
+            text.place(x=70, y=50 + everyheightline)
+            time = ttk.Label(text=f"{str(self.time)}", font=("Century Gothic", 10))
+            time.place(x=270, y=50 + everyheightline)
+            everyheightline += 25
 
 class UpperPartMesssenger:
     def __init__(self):
@@ -26,22 +39,22 @@ class UpperPartMesssenger:
 
 class BottomPartMesssenger:
     def __init__(self):
-        ttk.Entry().place(x=10, y=500)
-        ttk.Button(text='SEND').place(x=250, y=500)
+        ttk.Entry().place(x=10, y=500, height=40, width=220)
+        ttk.Button(text='SEND').place(x=250, y=500, height=40)
 
 class BottomToolbar:
     def __init__(self):
         settings = ttk.Button(text="MUSIC")
-        settings.place(x=10, y=550)
+        settings.place(x=10, y=555)
         profile = ttk.Button(text="CREATE NEW DIALOG", command=createNewDialog_messenger.newDialog)
-        profile.place(x=100, y=550)
+        profile.place(x=100, y=555)
         settings = ttk.Button(text="BACKUP")
-        settings.place(x=250, y=550)
+        settings.place(x=250, y=555)
 
 
 
 UpperPartMesssenger()
-AnyDialog('Alice', 'Hi, How\'s going on?', '14:17')
+AnyDialog('Alice', 'Hi, How\'s going on?', '14:17', 15)
 BottomPartMesssenger()
 BottomToolbar()
 
